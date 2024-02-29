0349faedc0a801f7f035eac48c004cce3856156e
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-01 01:49:30 +0200

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,15 +10,9 @@
   */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (tree == NULL)
 		return (0);
 	if (tree != NULL && tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL)
-		leaves += 1;
-	if (tree->right != NULL)
-		leaves += 1;
-	return (leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
