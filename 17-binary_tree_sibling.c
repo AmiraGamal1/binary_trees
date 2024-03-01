b911b865c39dc629c83cbeb2103097a1c37a20ef
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-03-01 03:33:07 +0200

    perfect

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+  * binary_tree_sibling - sibling of the nodes
+  *
+  * @node: root node
+  *
+  * Return: silbing node
+  */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+}
